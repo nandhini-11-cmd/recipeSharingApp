124c20cbd7766484d54e397d26abd5051147dbb9
nandhini-11-cmd <nandhini.ks11@gmail.com>
2025-07-18 19:03:55 +0530

    status change

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,7 +49,7 @@ exports.loginUser = async (req,res)=>{
 
         const token = await jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn:"7d"});
 ;
-        res.status(400).json({token, user:{id:user._id, name: user.name, email:user.email}});
+        res.status(200).json({token, user:{id:user._id, name: user.name, email:user.email}});
 
 
 
