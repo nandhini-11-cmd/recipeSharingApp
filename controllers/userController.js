1881a109adbfcb9432b3bd5a5e3643e531f7f3ad
nandhini-11-cmd <nandhini.ks11@gmail.com>
2025-07-21 19:18:06 +0530

    follow unfollow changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -65,23 +65,22 @@ exports.followUser = async (req, res) => {
 
 exports.unfollowUser = async (req, res) => {
   try {
-    const userToUnfollow = await User.findById(req.params.userId);
-    const currentUser = await User.findById(req.user._id);
+  const userToUnfollow = await User.findById(req.params.userId);
+  const currentUser = await User.findById(req.user._id);
 
-    if (!userToUnfollow) return res.status(404).json({ msg: "User not found" });
+  if (!userToUnfollow) return res.status(404).json({ msg: "User not found" });
 
-    currentUser.following = currentUser.following.filter(
-      id => !id.equals(userToUnfollow._id)
-    );
-    userToUnfollow.followers = userToUnfollow.followers.filter(
-      id => !id.equals(currentUser._id)
-    );
+  currentUser.following = currentUser.following.filter(
+    id => !id.equals(userToUnfollow._id)  
+  );
+  userToUnfollow.followers = userToUnfollow.followers.filter(
+    id => !id.equals(currentUser._id)   
+  );
 
-    await currentUser.save();
-    await userToUnfollow.save();
+  await currentUser.save();
+  await userToUnfollow.save();
 
-    res.json({ msg: "Unfollowed user successfully" });
-  } catch (err) {
-    res.status(500).json({ msg: err.message });
-  }
-};
+  res.json({ msg: "Unfollowed user successfully" });
+} catch (err) {
+  res.status(500).json({ msg: err.message });
+}};
